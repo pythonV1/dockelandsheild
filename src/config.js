3769633df41772b46040919b060c01372a54af5a
tutorialshore <benfitsvideos@gmail.com>
2024-11-12 23:07:41 +0530

    config chnage

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 // config.js
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api';
+const API_BASE_URL = 'http://ec2-13-202-221-203.ap-south-1.compute.amazonaws.com:8000/api';
 export default API_BASE_URL;
